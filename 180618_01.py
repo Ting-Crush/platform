53f2b8395772f664790e79ca4421de85b0cfafd1
Ting-Crush <40261179+Ting-Crush@users.noreply.github.com>
2018-07-29 01:36:45 +0900

    Add files via upload

---
+++ 180618_01.py
@@ -0,0 +1,58 @@
+﻿def foo():
+	print("hell : everyone!")
+
+
+
+def main():
+	celsius = eval(input("what is the celsius temperature?"))
+	fahrenheit = (9/5) * celsius +32
+	print("The temperature is ", fahrenheit, " degrees Fanhrenheits.")
+
+
+a = input()
+a*4
+
+a1 = eval(input())
+a1 * 4
+
+number = input("숫자를 입력하세요: ")
+
+number2 = eval(input("숫자를 입력하세요: "))
+
+
+x = 5
+x
+print(x)
+y= x*2 +10
+
+
+a=123
+print(a)
+a1= "python"
+print(a1)
+a = [1,2,3]
+
+
+print("lift" "id" " too hegayt")
+
+print("lift", "is", "too much")
+
+
+for i in range(10):
+	print(i, end= " ")
+
+
+for i in range(10):
+	print(i)
+
+
+def nini():
+	for i in range(10):
+		print(i, end='')
+
+nini()
+
+
+def nini2():
+	for i in range(10):
+		print(i, end='#')
